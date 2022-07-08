a1bd80932fa914ad77b90d7aeffccac217f94785
Michel Hoffmann <69813855+MichelHoffmann@users.noreply.github.com>
2022-07-08 16:32:20 -0500

    Iniciando a section quarto

---
+++ quarto/quarto.js
@@ -0,0 +1,34 @@
+var header = document.getElementById('navigation')
+var openbtn = document.getElementById('open-menu')
+var closebtn = document.getElementById('close-menu')
+var toTop = document.getElementById('backToTop')
+var corpo = document.getElementById('corpo')
+
+window.addEventListener('scroll', onScroll)
+openbtn.addEventListener('click', openMenu)
+closebtn.addEventListener('click', closeMenu)
+window.addEventListener('scroll', showButton)
+
+function onScroll() {
+  if (scrollY > 0) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  corpo.classList.add('open')
+}
+
+function closeMenu() {
+  corpo.classList.remove('open')
+}
+
+function showButton() {
+  if (scrollY > 100) {
+    toTop.classList.add('active')
+  } else {
+    toTop.classList.remove('active')
+  }
+}
